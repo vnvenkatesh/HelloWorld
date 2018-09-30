725d51beecd59aea4ead0a6e2c2f86967d7f8e76
Venkatesh Veluguri <vnvenaktesh@gmail.com>
2018-09-30 23:54:39 +0530

    Second Commit

--- HelloWorld/Form1.cs
+++ HelloWorld/Form1.cs
@@ -19,7 +19,7 @@ namespace HelloWorld
 
         private void button1_Click(object sender, EventArgs e)
         {
-            MessageBox.Show("Hello World! First push Venkat.");
+            MessageBox.Show("Hello World! First push Venkat. ");
             
         }
     }
