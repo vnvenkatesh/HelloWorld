13ec24e29ec1174ad9ca2c121a4b85715e314748
Venkatesh Veluguri <vnvenaktesh@gmail.com>
2018-10-01 02:25:09 +0530

    Tenth commit

--- HelloWorld/Form1.cs
+++ HelloWorld/Form1.cs
@@ -19,7 +19,7 @@ namespace HelloWorld
 
         private void button1_Click(object sender, EventArgs e)
         {
-            MessageBox.Show("Hello World! Ninth push Venkat. ");
+            MessageBox.Show("Hello World! Tenth push Venkat. ");
             
         }
     }
